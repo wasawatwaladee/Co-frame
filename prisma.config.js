d469e3b0220f2fec8e3b805fed46e6849970c9b3
wasawatwaladee <wasawatwaladee2@gmail.com>
2025-11-27 19:31:14 +0700

    uncomment prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -1,19 +1,19 @@
-// import "dotenv/config";
-// import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
-// export default defineConfig({
-//   schema: "prisma/schema.prisma",
-//    client: {
-//     adapter: "mysql",
-//     url: process.env.DATABASE_URL, // ต้องแน่ใจว่า .env มี DATABASE_URL
-//   },
-//   migrations: {
-//     path: "prisma/migrations",
-//   },
-//   datasource: {
-//     url: env("DATABASE_URL"),
-//   },
-// });
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+   client: {
+    adapter: "mysql",
+    url: process.env.DATABASE_URL, // ต้องแน่ใจว่า .env มี DATABASE_URL
+  },
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
 
 
 
