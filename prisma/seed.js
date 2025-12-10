37ecc944422523d02465167905dda51c6dae87b1
wasawatwaladee <wasawatwaladee2@gmail.com>
2025-12-10 13:40:54 +0700

    add communityCategory in seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -16,7 +16,7 @@ async function main() {
       thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Big_buck_bunny_poster_big.jpg',
       videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
       poster: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Big_buck_bunny_poster_big.jpg',
-      duration: null, // ใช้ null แทน 'NULL'
+      duration: 596, // ใช้ null แทน 'NULL'
       createdAt: new Date('2025-12-03T09:19:11.445Z'),
       updatedAt: new Date('2025-12-03T09:19:11.445Z'),
       categoryId: 1, // ⭐️ สมมติว่ามี categoryId เป็น 1
@@ -28,7 +28,7 @@ async function main() {
       thumbnail: 'https://images-cdn.ispot.tv/ad/7TK8/default-large.jpg',
       videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
       poster: 'https://images-cdn.ispot.tv/ad/7TK8/default-large.jpg',
-      duration: null,
+      duration: 15,
       createdAt: new Date('2025-12-03T09:19:11.446Z'),
       updatedAt: new Date('2025-12-03T09:19:11.446Z'),
       categoryId: 2, // ⭐️ สมมติว่ามี categoryId เป็น 2
@@ -40,6 +40,30 @@ async function main() {
       thumbnail: 'https://resizing.flixster.com/oo9VGItEyVU48bUnUO23gvP5mrA=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10875273_v_h9_ac.jpg',
       videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4',
       poster: 'https://resizing.flixster.com/oo9VGItEyVU48bUnUO23gvP5mrA=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10875273_v_h9_ac.jpg',
+      duration: 734,
+      createdAt: new Date('2025-12-03T09:19:11.446Z'),
+      updatedAt: new Date('2025-12-03T09:19:11.446Z'),
+      categoryId: 1, // ⭐️ สมมติว่ามี categoryId เป็น 1
+    },
+    {
+      id: 4,
+      title: 'Sintel',
+      description: 'Sintel, who embarks on a perilous journey across a harsh world to find the baby dragon she once rescued and befriended.',
+      thumbnail: 'https://studio.blender.org/files/cache/7d/81/7d81005541616e9f05aa992d508f19ff.jpg',
+      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
+      poster: 'https://studio.blender.org/files/cache/7d/81/7d81005541616e9f05aa992d508f19ff.jpg',
+      duration: null,
+      createdAt: new Date('2025-12-03T09:19:11.446Z'),
+      updatedAt: new Date('2025-12-03T09:19:11.446Z'),
+      categoryId: 1, // ⭐️ สมมติว่ามี categoryId เป็น 1
+    },
+    {
+      id: 5,
+      title: 'Elephants-dream',
+      description: 'Emo, a young and naive inhabitant, and Proog, an older resident, as they navigate a gigantic, surreal machine. he story explores themes of reality, imagination, and communication.',
+      thumbnail: 'https://studio.blender.org/files/cache/bc/94/bc94c954783f851cdfe471c3311e24d5.jpg',
+      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      poster: 'https://studio.blender.org/files/cache/bc/94/bc94c954783f851cdfe471c3311e24d5.jpg',
       duration: null,
       createdAt: new Date('2025-12-03T09:19:11.446Z'),
       updatedAt: new Date('2025-12-03T09:19:11.446Z'),
